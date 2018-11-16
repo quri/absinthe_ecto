da9017aa59e1dc5f59639b3950bfb0f5a303e556
Pol Miro <polmiro@gmail.com>
2018-11-16 12:58:46 -0800

    Upgrade for 1.5.0-alpha.0

--- mix.exs
+++ mix.exs
@@ -4,16 +4,18 @@ defmodule Absinthe.Ecto.Mixfile do
   @version "0.1.3"
 
   def project do
-    [app: :absinthe_ecto,
-     version: @version,
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     elixirc_paths: elixirc_paths(Mix.env),
-     package: package(),
-     source_url: "https://github.com/absinthe-graphql/absinthe_ecto",
-     docs: [source_ref: "v#{@version}", main: "Absinthe.Ecto"],
-     deps: deps()]
+    [
+      app: :absinthe_ecto,
+      version: @version,
+      elixir: "~> 1.3",
+      build_embedded: Mix.env() == :prod,
+      start_permanent: Mix.env() == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      package: package(),
+      source_url: "https://github.com/absinthe-graphql/absinthe_ecto",
+      docs: [source_ref: "v#{@version}", main: "Absinthe.Ecto"],
+      deps: deps()
+    ]
   end
 
   # Configuration for the OTP application
@@ -24,11 +26,13 @@ defmodule Absinthe.Ecto.Mixfile do
   end
 
   defp package do
-    [description: "GraphQL helpers for Absinthe",
-     files: ["lib", "priv", "mix.exs", "README*"],
-     maintainers: ["Bruce Williams", "Ben Wilson"],
-     licenses: ["MIT"],
-     links: %{github: "https://github.com/absinthe-graphql/absinthe_ecto"}]
+    [
+      description: "GraphQL helpers for Absinthe",
+      files: ["lib", "priv", "mix.exs", "README*"],
+      maintainers: ["Bruce Williams", "Ben Wilson"],
+      licenses: ["MIT"],
+      links: %{github: "https://github.com/absinthe-graphql/absinthe_ecto"}
+    ]
   end
 
   # Dependencies can be Hex packages:
@@ -42,11 +46,11 @@ defmodule Absinthe.Ecto.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:absinthe, "~> 1.3.0 or ~> 1.4.0"},
+      {:absinthe, "~> 1.3.0 or ~> 1.4.0 or ~> 1.5.0-alpha.0"},
       {:ecto, ">= 0.0.0"},
       {:ex_doc, ">= 0.0.0", only: [:dev]},
       {:postgrex, ">= 0.13.0", only: [:test]},
-      {:ex_machina, ">= 2.0.0", only: [:test]},
+      {:ex_machina, ">= 2.0.0", only: [:test]}
     ]
   end
 
